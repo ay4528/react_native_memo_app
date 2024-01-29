8c746d19d58fa7aa7c77cb6fe571231eda4c51f9
ayu <ayuabc4528@gmail.com>
2024-01-29 09:58:07 +0900

    Create Navigation

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -1,18 +1,16 @@
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native"
 import { Link, router } from "expo-router"
 
-import Header from "../../components/Header"
 import Button from "../../components/Button"
 
 const handlePress = (): void => {
 	// ログイン
-	router.push("/memo/list")
+	router.replace("/memo/list")
 }
 
 const LogIn = (): JSX.Element => {
 	return (
 		<View style={styles.container}>
-			<Header />
 			<View style={styles.inner}>
 				<Text style={styles.title}>Log In</Text>
 				<TextInput style={styles.input} value="Email address" />
--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -1,7 +1,6 @@
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native"
 import { Link, router } from "expo-router"
 
-import Header from "../../components/Header"
 import Button from "../../components/Button"
 
 const handlePress = (): void => {
@@ -12,7 +11,6 @@ const handlePress = (): void => {
 const SignUp = (): JSX.Element => {
 	return (
 		<View style={styles.container}>
-			<Header />
 			<View style={styles.inner}>
 				<Text style={styles.title}>Sign Up</Text>
 				<TextInput style={styles.input} value="Email address" />
